c4df6bde8a7b8abf11cb460e60417304b678341a
Eduardo Leite Ranzzani <eduranzzani@gmail.com>
2022-11-08 02:47:51 -0400

    Melhorias do Nível 03

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,90 +5,105 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
-    public float jumpForce;
+  public float speed;
+  public float jumpForce;
 
-    public bool isJumping;
-    public bool isDoubleJump;
+  public bool isJumping;
+  public bool isDoubleJump;
 
-    private new Rigidbody2D rigidbody2D;
-    private Animator animator;
+  private new Rigidbody2D rigidbody2D;
+  private Animator animator;
 
-    // Start is called before the first frame update
-    void Start()
+  public static PlayerController instance;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    rigidbody2D = GetComponent<Rigidbody2D>();
+    animator = GetComponent<Animator>();
+    instance = this;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    Move();
+    Jump();
+  }
+
+  void Move()
+  {
+    float horizontal = Input.GetAxis("Horizontal");
+    Vector3 movement = new(horizontal, 0f, 0f);
+    transform.position += speed * Time.deltaTime * movement;
+
+    if (horizontal > 0f)
     {
-        rigidbody2D = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
+      animator.SetBool("Walk", true);
+      transform.eulerAngles = new Vector3(0f, 0f, 0f);
     }
-
-    // Update is called once per frame
-    void Update()
+    else if (horizontal < 0f)
     {
-        Move();
-        Jump();
+      animator.SetBool("Walk", true);
+      transform.eulerAngles = new Vector3(0f, 180f, 0f);
     }
-
-    void Move()
+    else
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        Vector3 movement = new(horizontal, 0f, 0f);
-        transform.position += speed * Time.deltaTime * movement;
+      animator.SetBool("Walk", false);
+    }
+  }
 
-        if (horizontal > 0f)
+  void Jump()
+  {
+    if (Input.GetButtonDown("Jump"))
+    {
+      if (!isJumping)
+      {
+        rigidbody2D.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        animator.SetBool("Jump", true);
+        isDoubleJump = true;
+      }
+      else
+      {
+        if (isDoubleJump)
         {
-            animator.SetBool("Walk", true);
-            transform.eulerAngles = new Vector3(0f, 0f, 0f);
-        } else if(horizontal < 0f)
-        {
-            animator.SetBool("Walk", true);
-            transform.eulerAngles = new Vector3(0f, 180f, 0f);
-        } else
-        {
-            animator.SetBool("Walk", false);
+          animator.SetTrigger("DoubleJump");
+          rigidbody2D.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+          isDoubleJump = false;
         }
+      }
     }
+  }
 
-    void Jump()
+  void OnCollisionEnter2D(Collision2D collision)
+  {
+    if (collision.gameObject.layer == 8)
     {
-        if (Input.GetButtonDown("Jump"))
-        {
-            if (!isJumping)
-            {
-                rigidbody2D.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
-                animator.SetBool("Jump", true);
-                isDoubleJump = true;
-            }
-            else
-            {
-                if (isDoubleJump)
-                {
-                    animator.SetTrigger("DoubleJump");
-                    rigidbody2D.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
-                    isDoubleJump = false;
-                }
-            }
-        }
+      isJumping = false;
+      animator.SetBool("Jump", false);
     }
+    else if (collision.gameObject.CompareTag("Spike"))
+    {
+      PlayerDie();
+    }
+    else if (collision.gameObject.CompareTag("Saw"))
+    {
+      PlayerDie();
+    }
+  }
 
-    void OnCollisionEnter2D(Collision2D collision)
+  void OnCollisionExit2D(Collision2D collision)
+  {
+    if (collision.gameObject.layer == 8)
     {
-        if (collision.gameObject.layer == 8)
-        {
-            isJumping = false;
-            animator.SetBool("Jump", false);
-        } else if (collision.gameObject.CompareTag("Spike"))
-        {
-            animator.SetTrigger("Die");
-            GameController.instance.ShowGameOver();
-            Destroy(gameObject, 0.5f);
-        }
+      isJumping = true;
     }
+  }
 
-    void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.layer == 8)
-        {
-            isJumping = true;
-        }
-    }
+  void PlayerDie()
+  {
+    animator.SetTrigger("Die");
+    GameController.instance.ShowGameOver();
+    Destroy(gameObject, 0.5f);
+  }
 }
--- Assets/Scripts/SawController.cs
+++ Assets/Scripts/SawController.cs
@@ -4,9 +4,29 @@ using UnityEngine;
 
 public class SawController : MonoBehaviour
 {
-
   public float speed;
-  private bool directionRight;
+  public float moveTime;
+  private bool directionRight = true;
   private float timer;
+
+  private void Update()
+  {
+    if (directionRight)
+    {
+      transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+    else
+    {
+      transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+
+    timer += Time.deltaTime;
+
+    if (timer >= moveTime)
+    {
+      directionRight = !directionRight;
+      timer = 0f;
+    }
+  }
 
 }
